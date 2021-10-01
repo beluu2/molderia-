0d29cbdb596a16db2ee60e929371d0465a9d1967
Belu <Belu@queti>
2021-10-01 15:44:23 -0300

    agregado practica 29.9

---
+++ prueba0/practica  29.9.py
@@ -0,0 +1,9 @@
+for n in range(10):
+	print (str(n) + " Oveja" + ("s" if n != 1 else "")) 
+
+for n in range(10):
+	if n != 1: 
+		print(str(n) + " Oveja")
+	else: 
+		print(str(n) + " Ovejas")
+			
